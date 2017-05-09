957b30c778a9ddb1b5b15f07818b3826f5f6324c
Daniel Wangai <daniel.maina@andela.com>
2017-05-09 14:51:48 +0300

    [Feature] Add bucketlist item interface.

--- src/app/models/bucketlist.ts
+++ src/app/models/bucketlist.ts
@@ -1,7 +1,9 @@
+import { IItem } from './item';
+
 export interface IBucketlist {
   id: number;
   name: string;
-  items: any[];
+  items: IItem[];
   created_by: number;
   modified_at: string;
   created_at: string;
---
+++ src/app/models/item.ts
@@ -0,0 +1,8 @@
+export interface IItem {
+  id: number;
+  name: string;
+  description: string;
+  bucketlist_id: number;
+  created_by: number;
+  modified_at: string;
+}
