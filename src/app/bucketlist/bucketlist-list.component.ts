c308279a9a94b72794112fa28df7ae5b97174837
Daniel Wangai <daniel.maina@andela.com>
2017-05-10 10:52:27 +0300

    [Feature] Add bucketlist component.

--- src/app/bucketlist/bucketlist-list.component.ts
+++ src/app/bucketlist/bucketlist-list.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { Component, OnInit } from '@angular/core';
+import {IBucketlist} from '../models/bucketlist';
+import { BucketlistService } from './all-bucketlists.service';
 
 @Component({
   selector: 'bucket-list',
   moduleId: module.id,
   templateUrl: './bucketlist-list.component.html',
 })
-export class BucketlistListComponent {
+export class BucketlistListComponent implements OnInit {
+  bucketlists: IBucketlist[];
+  errorMessage: string;
+  addBucket: string = '';
 
+  constructor (
+    private bucketlistService: BucketlistService
+  ) {}
+
+  ngOnInit(): void {
+    this.getBucketlist();
+  }
+
+  getBucketlist() {
+    this.bucketlistService.getBucketlists()
+      .subscribe((bucketlists: IBucketlist[]) => {
+          this.bucketlists = bucketlists;
+          console.log(bucketlists);
+        },
+        (error: any) => this.errorMessage = <any>error
+      );
+  }
+
+  createBucketlist(name: string): void {
+    name = name.trim();
+    if (!name) {
+      console.log('name required');
+      return;
+    }
+    this.bucketlistService.createBucketlist(name).subscribe(
+      () => this.getBucketlist()
+    );
+  }
 }
