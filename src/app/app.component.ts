c221b0390b3b88558fb44bde393215d105955158
Daniel Wangai <daniel.maina@andela.com>
2017-05-10 19:38:36 +0300

    [Chore] Add logout method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './user/authentication.service';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent  {
   appName = 'Angular II';
   myBuckets: string;
 
+  constructor( 
+    private authService: AuthenticationService 
+    ) {
+
+  }
+  logout() {
+    this.authService.logout();
+  }
   onClickGet() {
     this.myBuckets = 'Button Clicked';
   }
