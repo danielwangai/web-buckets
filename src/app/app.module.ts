b4264ecfaba1a5adc60cdf38a98796c4ef4bc831
Daniel Wangai <daniel.maina@andela.com>
2017-05-10 10:44:31 +0300

    [Feature] Add routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,52 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import  { BucketlistListComponent } from './bucketlist/bucketlist-list.component';
+import { BucketlistService } from './bucketlist/all-bucketlists.service';
+
+// services
+import { AuthenticationService } from './user/authentication.service';
+
+// components
+import { LoginComponent } from './user/login/login.component';
+import { ItemComponent } from './bucketlist/item.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'buckelist',
+    // canActivate: [],
+    component: BucketlistListComponent,
+    children: [
+      {
+        path: ':id',
+        component: ItemComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, BucketlistListComponent ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    BucketlistListComponent,
+    LoginComponent,
+    ItemComponent
+  ],
+  providers: [ BucketlistService, AuthenticationService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
