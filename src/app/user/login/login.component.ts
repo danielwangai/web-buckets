95a0ba1daddae7af11946e1a3140b1439e1a4925
Daniel Wangai <daniel.maina@andela.com>
2017-05-10 10:43:09 +0300

    [Feature] Add login component.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,3 +1,41 @@
-/**
- * Created by danielmaina on 09/05/2017.
- */
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'login',
+  moduleId: module.id,
+  templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  error = '';
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
+
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
+  }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+    .subscribe(result => {
+      console.log(result);
+      if (result === true) {
+        // login successful
+        this.router.navigate(['']);
+      } else {
+        // login failed
+        this.error = 'Username or password is incorrect';
+        this.loading = false;
+      }
+    });
+  }
+}
