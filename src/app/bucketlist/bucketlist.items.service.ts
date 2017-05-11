8405bc6786ca6f4c118a7fb89311d67b8764bd11
Daniel Wangai <daniel.maina@andela.com>
2017-05-11 12:56:34 +0300

    [Feature] Add service method to delete item.

--- src/app/bucketlist/bucketlist.items.service.ts
+++ src/app/bucketlist/bucketlist.items.service.ts
@@ -20,9 +20,14 @@ export class BucketlistItemService {
         return this._handleGetRequest(this.bucketlistAPIUrl + '/api/v1/bucketlists/'+id+'/items');
     }
 
+    deleteBucketlistItem(bucketlist_id: number, item_id: number): Observable<IItem> {
+      return this._handleDeleteRequest(this.bucketlistAPIUrl + 
+        '/api/v1/bucketlists/'+bucketlist_id+'/items/'+ item_id);
+    }
+
     private _handleGetRequest(url: string) {
         let { token } = JSON.parse(localStorage.getItem('currentUser'));
-        console.log(token);
+        // console.log(token);
 
         const headers = new Headers({
         'Content-Type': 'application/json',
@@ -35,6 +40,21 @@ export class BucketlistItemService {
         return this.http.get(url, options)
             .map(this.extractData)
             .catch(this.handleError)
+    }
+
+    private _handleDeleteRequest(url: string) {
+      let { token } = JSON.parse(localStorage.getItem('currentUser'));
+        const headers = new Headers({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': token
+        });
+
+        const options = new RequestOptions({ headers });
+
+        return this.http.delete(url, options)
+          .map(this.extractData)
+          .catch(this.handleError)
     }
 
     private extractData(res: Response) {
